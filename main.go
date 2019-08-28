b184dfd735b048659f9720bbf465337fc8bdcfd4
chjintao163@163.com <chjintao163@163.com>
2019-08-28 11:09:25 +0800

    delete ;

--- main.go
+++ main.go
@@ -5,5 +5,6 @@ import(
 )
 
 func Init(){
-	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest");
+	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
+	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
 }
