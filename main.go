dc264678b106ff7c914846ee74d02e4ef79a3f6d
chjintao163@163.com <chjintao163@163.com>
2019-08-28 12:17:35 +0800

    del func Init()

--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import(
 	"fmt"
 )
 
-
+/*
 func Init(){
 	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
 	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
 	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
 }
+*/
 
 
-/*
+
 func GoModTest(){
 	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
 	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
+	fmt.Println( "You successfully get package github.com/Capri-Tu/gomodTest")
 }
-*/
